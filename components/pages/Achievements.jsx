3f1f717d077461a5273f12807b7fb578a41068b9
Husnain <imhuss22@gmail.com>
2022-07-07 00:12:33 +0100

    minor changes

--- components/pages/Achievements.jsx
+++ components/pages/Achievements.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleUp, faAngleDown, faUserPlus, faQuestionCircle, faRobot } from '@fortawesome/free-solid-svg-icons';
+import { faAngleUp, faAngleDown, faEnvelopeOpen, faUserPlus, faQuestionCircle, faRobot } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
 
 export default function AchievementsPage() {
@@ -12,39 +12,94 @@ export default function AchievementsPage() {
                     <div className="mx-12 bg-zinc-900 rounded-xl mt-8 p-6 h-[65%] md:min-w-full lg:min-w-full">
                         <center>
                         <p className="text-4xl text-slate-200 font-bold m-2">
+                        <FontAwesomeIcon className="mr-6" icon={faEnvelopeOpen}></FontAwesomeIcon><u>Enrolments</u>
+                        </p>
+                        </center><br></br>
+                        <div className="row">
+                      <div className="bg-dark-new flex max-w-md shadow-lg rounded-lg overflow-hidden">
+    <div className="w-1/3 bg-cover bg-landscape"><br></br><br></br>
+    <Image
+      style={{textAlign:"center"}}
+      alt="RAF Air Cadet" height="100" width="100" src="/assets/img/rafatc.png"
+      />
+    </div>
+    <div className="w-2/3 p-4">
+        <h1 className="font-bold text-2xl text-gray-400 dark:text-gray-100">
+            RAF Air Cadets
+        </h1>
+        <p className="mt-2 text-gray-500 text-sm">
+        Recently joined RAF Air Cadets.. I think I&apos;m on a trial-period for now, why don&apos;t you join in the mean-time?
+        </p>
+        <div className="flex item-center mt-2">
+        </div>
+        <div className="flex item-center justify-between mt-3">
+            <a href="https://www.raf.mod.uk/aircadets/"><button className="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">
+                Learn More
+            </button></a>
+        </div>
+    </div>
+</div>
+
+&nbsp;&nbsp;&nbsp;&nbsp;
+
+<div className="bg-dark flex max-w-md shadow-lg rounded-lg overflow-hidden">
+    <div className="w-1/3 bg-cover bg-landscape"><br></br><br></br>
+    <Image
+      style={{textAlign:"center"}}
+      alt="Early verified bot developer" height="100" width="100" src="/assets/img/WMPCadets.png"
+      />
+    </div>
+    <div className="w-2/3 p-4">
+        <h1 className="font-bold text-2xl text-gray-400 dark:text-gray-100">
+            WMP Cadets
+        </h1>
+        <p className="mt-2 text-gray-600 text-sm">
+          Currently enrolled into the cadets scheme, learning new skills on a weekly! As-well as participating positively in the community.
+        </p>
+        <div className="flex item-center mt-2">
+        </div>
+        <div className="flex item-center justify-between mt-3">
+            <a href="https://vpc.police.uk/"><button className="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">
+                Visit Volunteer Police Cadets Website
+            </button></a>
+        </div>
+    </div>
+</div>
+</div>
+
+                        <center>
+                        <p className="text-4xl text-slate-200 font-bold m-2">
                             <u><FontAwesomeIcon className="mr-6" icon={faUserPlus}></FontAwesomeIcon>
                             Achievements</u>
                         </p>
                         </center><br></br>           
                   
-                  
-                  <div className="row">
+                 <div className="row">
 
-                 
-                  <div className="column">
+                   <div className="column">
                     <div className="col-xs-12 col-sm-6">
                       <div className="certificate-item clearfix">
                         <div className="certi-logo">
                         <Image
       style={{textAlign:"center"}}
-      alt="RAF Air Cadet" height="100" width="100" src="/assets/img/rafatc.png"
+      alt="Idley" height="100" width="100" src="/assets/img/idley.png"
       />
                         </div>
                         
                         <div className="certi-content">
                           <div className="certi-title">
-                            <h4>RAF Air Cadets</h4>
+                            <h4>Idley Host</h4>
                           </div>
                           <div className="certi-id">
-                            <span> <a href="https://www.raf.mod.uk/aircadets/"><button className="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">
-                Learn More
+                            <span> <a href="http://web.archive.org/web/20210924021613/https://idley.gg/https://www.raf.mod.uk/aircadets/"><button className="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">
+                Visit Archive
             </button></a></span>
                           </div>
                           <div className="certi-date">
-                            <span>13 June 2022</span>
+                            <span>Sadly <b>Archived</b> in January 2022</span>
                           </div>
                           <div className="certi-company">
-                            <span>Company: N/A</span>
+                            <span>Development: Idle</span>
                           </div>
                         </div>
                       </div>

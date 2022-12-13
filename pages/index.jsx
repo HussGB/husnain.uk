2597c8568723ef878c2559127e39ece1212fd54f
Husnain <imhuss22@gmail.com>
2022-12-13 23:17:16 +0000

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,8 +4,7 @@ import AchievementsPage from '../components/pages/Achievements.jsx';
 export default function IndexPage() {
   return (
     <>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6734476030952556"
-     crossorigin="anonymous"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6734476030952556"></script>
       <HomePage />
       <AchievementsPage />
     </>
